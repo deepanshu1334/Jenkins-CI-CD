f554ec6ecbbea279dfc5d3a4f46323ae2f1a09e0
deepanshu kumar <dk0962422@gmail.com>
2025-03-07 21:03:53 +0530

    update commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My Web App</h1>
+      <p>This is the frontend!</p>
+    </div>
+  );
+}
+
+export default App;
